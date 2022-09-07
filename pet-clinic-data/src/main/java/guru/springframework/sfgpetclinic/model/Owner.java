29eb84359a8734ce6ffa1c7b164fcc32a6ab46c0
vakt159 <vakt159@gmail.com>
2022-09-07 13:52:15 +0300

    13165

--- pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/model/Owner.java
+++ pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/model/Owner.java
@@ -1,4 +1,14 @@
 package guru.springframework.sfgpetclinic.model;
 
-public class Owner extends Person{
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.Set;
+@Getter
+@Setter
+public class Owner extends Person {
+    private Set<Pet> pets;
+    private String address;
+    private String city;
+    private String telephone;
 }
