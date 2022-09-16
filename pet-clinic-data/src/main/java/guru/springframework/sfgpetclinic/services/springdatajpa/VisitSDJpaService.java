ae54f6d0ae26df06a275905dee09060522aa7e30
vakt159 <vakt159@gmail.com>
2022-09-17 00:34:38 +0300

    Added VisitSDJpaService

---
+++ pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/services/springdatajpa/VisitSDJpaService.java
@@ -0,0 +1,47 @@
+package guru.springframework.sfgpetclinic.services.springdatajpa;
+
+import guru.springframework.sfgpetclinic.model.Vet;
+import guru.springframework.sfgpetclinic.model.Visit;
+import guru.springframework.sfgpetclinic.repositories.VisitRepository;
+import guru.springframework.sfgpetclinic.services.VisitService;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class VisitSDJpaService implements VisitService {
+
+    private final VisitRepository visitRepository;
+
+    public VisitSDJpaService(VisitRepository visitService) {
+        this.visitRepository = visitService;
+    }
+
+
+    @Override
+    public Set<Visit> findAll() {
+        Set<Visit> visits= new HashSet<>();
+        visitRepository.findAll().forEach(visit -> visits.add(visit));
+        return visits;
+    }
+
+    @Override
+    public Visit findById(Long aLong) {
+        return visitRepository.findById(aLong).orElse(null);
+    }
+
+    @Override
+    public Visit save(Visit object) {
+        return visitRepository.save(object);
+    }
+
+    @Override
+    public void delete(Visit object) {
+        visitRepository.delete(object);
+    }
+
+    @Override
+    public void deleteById(Long aLong) {
+        visitRepository.deleteById(aLong);
+    }
+
+}
